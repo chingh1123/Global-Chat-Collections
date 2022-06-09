ff512157edd9c022e62e4460cfc1862df61b9fa6
GH <86087456+chingh1123@users.noreply.github.com>
2022-06-10 00:44:51 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const {
+  Client,
+  Collection,
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton,
+  MessageAttachment,
+  Intents
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+  ],
+});
+module.exports = client;
