7d0f1c5bb1e66d23e314e5552766d3f3230792d1
GH <86087456+chingh1123@users.noreply.github.com>
2022-06-10 00:55:32 +0800

    Update Client (Crosspost GlobalChat).js

--- GlobalChat #2/Client (Crosspost GlobalChat).js
+++ GlobalChat #2/Client (Crosspost GlobalChat).js
@@ -1,12 +1,13 @@
 const client = require('../index.js')
+
 client.on('messageCreate', m => {
   if (m.author.bot) return;
   let url = ""
   m.attachments.forEach(a => {
         url = a.attachment
   });
-  const channel1 = client.channels.cache.get('984487882477674566')
-  const channel2 = client.channels.cache.get('984487905579900938')
+  const channel1 = client.channels.cache.get('CHANNEL_ID1')
+  const channel2 = client.channels.cache.get('CHANNEL_ID2')
 
   if (url === ''){
       if (m.channel.id === channel1.id) return channel2.send(`${m.author.tag}: \n>>> ${m.content}`)
