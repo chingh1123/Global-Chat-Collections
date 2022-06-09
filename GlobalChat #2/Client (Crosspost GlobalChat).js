25a8d66de88e094aae118d11ad8438da783f2b01
GH <86087456+chingh1123@users.noreply.github.com>
2022-06-10 00:42:07 +0800

    Create Client (Crosspost GlobalChat).js

---
+++ GlobalChat #2/Client (Crosspost GlobalChat).js
@@ -0,0 +1,17 @@
+client.on('messageCreate', m => {
+  if (m.author.bot) return;
+  let url = ""
+  m.attachments.forEach(a => {
+        url = a.attachment
+  });
+  const channel1 = client.channels.cache.get('984487882477674566')
+  const channel2 = client.channels.cache.get('984487905579900938')
+
+  if (url === ''){
+      if (m.channel.id === channel1.id) return channel2.send(`${m.author.tag}: \n>>> ${m.content}`)
+      if (m.channel.id === channel2.id) return channel1.send(`${m.author.tag}: \n>>> ${m.content}`)
+  } else {
+    if (m.channel.id === channel1.id) return channel2.send({ content: `${m.author.tag}: \n>>> ${m.content}`, files: [url] })
+    if (m.channel.id === channel2.id) return channel1.send({ content: `${m.author.tag}: \n>>> ${m.content}`, files: [url] })
+  }
+})
